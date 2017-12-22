4c6121192734354a95806789a7a4a54412b41458
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-22 20:49:40 +0100

    'richel_test' is a minimal reprex for the error

--- Tracker/richel_test.py
+++ Tracker/richel_test.py
@@ -10,11 +10,20 @@ source = "3f_1.mp4"
 cap = cv2.VideoCapture(source)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+if cap is None:
+  # print 'Warning: unable to open video source: ', source
+  # if fallback is not None:
+  #    return create_capture(fallback, None)
+  print "Error: cap is None"
+  raise SystemExit
+
+
 if cap is None or not cap.isOpened():
   # print 'Warning: unable to open video source: ', source
   # if fallback is not None:
   #    return create_capture(fallback, None)
-  print "Error: unable to open video source: '" + source + "'"
+  print "Error: cap is not opened"
   raise SystemExit
 
 print "Gelukt!"
