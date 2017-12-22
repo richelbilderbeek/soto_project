66292e8c83852c58270251dc48f331a3f5c504e6
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-22 21:55:09 +0100

    run_arduino added

--- Tracker/richel_test.py
+++ Tracker/richel_test.py
@@ -19,7 +19,7 @@ if cap is None:
   raise SystemExit
 
 
-if cap is None or not cap.isOpened():
+if cap.isOpened():
   # print 'Warning: unable to open video source: ', source
   # if fallback is not None:
   #    return create_capture(fallback, None)
